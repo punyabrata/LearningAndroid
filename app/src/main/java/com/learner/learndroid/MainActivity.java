3d6e3678271bc053af5d09f99e58f023d0e77388
Punyabrata Dhar <rajasthancatgray@gmail.com>
2018-12-10 20:23:18 -0600

    Task-RecyclerView_ViewModel: Refactored the view (activity) and the recycler view adapter.

--- app/src/main/java/com/learner/learndroid/MainActivity.java
+++ app/src/main/java/com/learner/learndroid/MainActivity.java
@@ -1,12 +1,15 @@
 package com.learner.learndroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -15,110 +18,30 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     /**
-     * List of product names.
+     * View model object.
      */
-    private ArrayList<String> productNames = new ArrayList<>();
-    /**
-     * List of product image URLs.
-     */
-    private ArrayList<String> productImageURLs = new ArrayList<>();
-    /**
-     * List of product descriptions.
-     */
-    private ArrayList<String> productDescriptions = new ArrayList<>();
-    /**
-     * List of product original prices.
-     */
-    private ArrayList<String> productOriginalPrices = new ArrayList<>();
-    /**
-     * List of product deal prices.
-     */
-    private ArrayList<String> productDealPrices = new ArrayList<>();
-    /**
-     * List of product you save texts.
-     */
-    private ArrayList<String> productYouSaveTexts = new ArrayList<>();
+    private ItemViewModel itemViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Activity on create");
-        initRecyclerViewItems();
-        initRecyclerView();
+
+        itemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
+        itemViewModel.getAllProducts().observe(this, new Observer<List<Product>>() {
+            @Override
+            public void onChanged(@Nullable List<Product> products) {
+                updateRecyclerView(products);
+            }
+        });
     }
 
-    private void initRecyclerView() {
+    private void updateRecyclerView(List<Product> products) {
         Log.d(TAG, "Recycler view is being set up.");
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(productNames, productImageURLs,
-                productDescriptions, productOriginalPrices, productDealPrices, productYouSaveTexts, this);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(products,this);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    private void initRecyclerViewItems() {
-        productImageURLs.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        productNames.add("Apple Macbook Pro");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        productNames.add("Dell Laptop 16 Inch");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        productNames.add("Asus Laptop");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/j6myfqglup501.jpg");
-        productNames.add("Apple Macbook Super");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        productNames.add("Lenovo Laptop 14 Inch");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/k98uzl68eh501.jpg");
-        productNames.add("IBM Thinkpad");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/glin0nwndo501.jpg");
-        productNames.add("Google Chromebook");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.redd.it/obx4zydshg601.jpg");
-        productNames.add("Sony Vio");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
-
-        productImageURLs.add("https://i.imgur.com/ZcLLrkY.jpg");
-        productNames.add("Apple Macbook Air");
-        productDescriptions.add("Very cool laptop to use");
-        productOriginalPrices.add("$1000 USD");
-        productDealPrices.add("$500 USD");
-        productYouSaveTexts.add("You save $500!");
     }
 }
