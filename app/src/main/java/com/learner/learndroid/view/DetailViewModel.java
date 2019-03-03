0cfdac1a8ce213ddc52e47291bc5ccc296b033a2
Punyabrata Dhar <rajasthancatgray@gmail.com>
2019-03-02 18:22:45 -0600

    Task_Detail_View_View_Model: The ViewModel queries the item from the repository.

--- app/src/main/java/com/learner/learndroid/view/DetailViewModel.java
+++ app/src/main/java/com/learner/learndroid/view/DetailViewModel.java
@@ -2,17 +2,32 @@ package com.learner.learndroid.view;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.learner.learndroid.entity.trending.Item;
+import com.learner.learndroid.repository.ProductRepository;
 
 public class DetailViewModel extends AndroidViewModel {
+
+    /**
+     * Tag for debugging.
+     */
+    private static final String TAG = "DetailViewModel";
 
     /**
      * Live data to hold the product.
      */
     private MutableLiveData<Item> liveData = new MutableLiveData<>();
+
+    /**
+     * An instance of product repository.
+     */
+    private ProductRepository repository = null;
 
     /**
      * Constructor.
@@ -23,5 +38,36 @@ public class DetailViewModel extends AndroidViewModel {
         super(application);
     }
 
+    /**
+     * Sets product repository to the view model .
+     *
+     * @param productRepository {@link ProductRepository}
+     */
+    public void setRepository(ProductRepository productRepository) {
+        this.repository = productRepository;
+    }
 
+    /**
+     * Gets the products using the {@link DetailViewModel#fetchItem(String)} private method.
+     * @return LiveData of an item.
+     */
+    public LiveData<Item> getItem(String itemId) {
+        if (liveData.getValue() == null) {
+            fetchItem(itemId);
+        }
+        return liveData;
+    }
+
+    /**
+     * Fetches the data from the repository.
+     */
+    private void fetchItem(String id) {
+        Log.d(TAG, "Fetch Data");
+        repository.getItem(id).observeForever(new Observer<Item>() {
+            @Override
+            public void onChanged(@Nullable Item item) {
+                liveData.setValue(item);
+            }
+        });
+    }
 }
